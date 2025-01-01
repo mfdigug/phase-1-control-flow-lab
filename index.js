c9a85d8d336382decf470a9b45d77d000ba569aa
Michelle Di Guglielmo <mfdigug@gmail.com>
2025-01-01 14:24:06 +1100

    done with first function

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(rideDistance){
+  if (rideDistance <= 400)
+    return "This one is on me!";
+  else if (rideDistance > 400 && rideDistance < 2000) 
+    return "That will be twenty bucks.";
+  else if (rideDistance > 2000 && rideDistance <= 2500)
+    return "I will gladly take your thirty bucks.";
+  else
+    return "No can do.";
 }
 
 function ternaryCheckCity(){
