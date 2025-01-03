40765c08b659dda61da782d5a80e909ecdec4718
Michelle Di Guglielmo <mfdigug@gmail.com>
2025-01-03 11:20:47 +1100

    done with second two functions

--- index.js
+++ index.js
@@ -9,10 +9,19 @@ function scuberGreetingForFeet(rideDistance){
     return "No can do.";
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city){
+  return city === 'NYC' ? "Ok, sounds good." : "No go.";
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+
+function switchOnCharmFromTip(tip){
+  switch(tip) {
+    case 'generous':
+    return 'Thank you so much.';
+    case'not as generous':
+    return 'Thank you.';
+    default:
+     return 'Bye.';
+    }
+
 }
